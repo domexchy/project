ae3dc07597c1fa00b0c4b4fa4c3e47a89e687185
domexchy <168428725+domexchy@users.noreply.github.com>
2024-05-14 04:27:36 -0700

    Add files via upload

---
+++ jumia.js
@@ -0,0 +1,9 @@
+function toogleContent() {
+    var content = document.getElementById("content");
+    var btn =
+    document.getElementById("showMoreBtn");
+    if (conten.style.display === "none";)
+}
+
+content.style.display = "block";
+btn.textContent = "Show less";
